f7f5d910a22126ad238861869db5ae0b90ef6e30
ibrahim <ibrahim.taouaou@hotmail.com>
2024-03-03 13:20:22 +0100

    add clear storage

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import TabManagment from "./features/tabs/TabManagment";
 import VerticalTabs from "./features/tabs/Tabs";
 
 function App() {
+  function handleClear() {
+    localStorage.clear();
+  }
   return (
     <div className="flex flex-col bg-white-bg h-[100vh] divide-y-2">
+      <button onClick={handleClear}>Clear Storage</button>
       <h1 className="text-xl text-white-bg bg-blue-dark font-semibold w-full self-center py-4 text-center">
         Free Your Mind
       </h1>
